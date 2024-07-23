d635ca58fa861e3530cec441675b1f7145421395
SSC369 <sai9821c@gmail.com>
2024-07-23 19:18:23 +0530

    plan api changes

--- backend/routes/planRoutes.js
+++ backend/routes/planRoutes.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 
 const fetchUser = require("../middlewares/fetchUser");
-const { addPlan, editPlan, getPlan } = require("../controllers/planController");
+const { addPlan,  getPlan } = require("../controllers/planController");
 
 router.post("/addplan", fetchUser, addPlan);
-router.put("/editplan", fetchUser, editPlan);
 router.get("/getplan/:date", fetchUser, getPlan);
 
 module.exports = router;
